f4a84814cfce6118293c362ec5a1691d55080c2d
ignaciojavier27 <nacholin124@gmail.com>
2024-12-01 13:47:16 -0300

    Validaciones con expresiones regulares

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,10 +9,34 @@ const initialForm = {
 
 const validationsForm = (form) => {
     let errors = {};
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    let regexComments = /^.{1,255}$/;
 
     if(!form.name.trim()){
         errors.name = "El campo nombre es requerido";
+    }else if(!regexName.test(form.name.trim())){
+        errors.name = "El campo nombre solo acepta letras y espacios en blanco";
     }
+
+    if(!form.email.trim()){
+        errors.email = "El campo email es requerido";
+    }else if(!regexEmail.test(form.email.trim())){
+        errors.email = "El campo email es incorrecto";
+    }
+
+    if(!form.subject.trim()){
+        errors.subject = "El campo asunto es requerido";
+    }
+
+    if(!form.comments.trim()){
+        errors.comments = "El campo comentarios es requerido";
+    }else if(!regexComments.test(form.comments.trim())){
+        errors.name = "El campo comentarios no debe exceder los 255 caracteres";
+    }
+
+
+
 
     return errors;
 };
